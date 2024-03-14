d1f435c3ed9b065291feca36eb6df4e9835f07f4
Claudio9616 <clares9611@gmail.com>
2024-03-14 17:34:37 +0100

    problem with save

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+    protected $fillable = ['tile', 'description', 'thumb', 'price', 'series', 'sale-date'];
 }
